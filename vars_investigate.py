fc90d06958f9ec2ead67a51e53e7210f5e8184fc
Gabireld <353029259@educ.dpcdsb.org>
2026-02-10 12:37:33 -0500

    Changed value of all variables and added new variables with more text in the print function.

---
+++ vars_investigate.py
@@ -0,0 +1,37 @@
+"""
+author: Gabriel Diab
+date: Tuesday, February 10th, 2026
+Investigating Variables
+"""
+
+# Input
+name = "Jaquavius"
+grade = 15
+favourite_exclamation = "Ahlie?"
+least_favourite_colour = "Green"
+is_cool = True
+math_test_score = 67.7
+sister_name = "Ayeisha"
+teacher_name = "Gabagool"
+favourite_hobby="gooning"
+school_team_on= "Jamaican Dogeball Hide and Seek"
+
+slope = 6
+y_intercept = 7
+x = 67
+y = slope * x + y_intercept
+
+# Processing / Output
+print(
+    f"Hey guys, my name is {name} and I am currently in grade {grade}. "
+    + f"Yesterday, there was a spider in the classroom that made me yell {favourite_exclamation} whiile I was taking a math test with my {least_favourite_colour} pen :/ "
+    + f"One of the questions on the test was wether or not I thought Elvis was cool, so I obviously put {is_cool} to get the answer right... "
+    + f"Turns out that I only scored a {math_test_score} on the test. One of the qusetions was asking me to write the equation for a line "
+    + f"with a slope of {slope} and a y-intercept of {y_intercept} so I wrote y = {slope}x + {y_intercept}, but I did not notice the x value of {x} in the question "
+    + f"so I needed to, on the next line, say that the value of y was {y}. "
+    + f"I told my sister {sister_name} about this and she just laughed at me 🙄"
+    + "Anyway, that's the story of my math test."
+    + f"But also, Coach {teacher_name} called me in to his office after school on the anouncements. It was scary."
+    + f"Coach {teacher_name} told me that he caught me {favourite_hobby} many times on school property during the school day."
+    + f"Coach told me if I kept {favourite_hobby}, then I would be kicked off the {school_team_on} team."
+)
